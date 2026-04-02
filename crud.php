d0ed6011977a86827a90a01efbef24fb6c7968b3
infinitebolas <rouinsardneven@gmail.com>
2026-04-02 11:49:33 +0200

    critiques likées

--- crud.php
+++ crud.php
@@ -1,26 +1,44 @@
 <?php
-    
-class Critiques{
-    
-    public function Afficher(int $user_id){
-        require_once('db.php');
-        $db_critiques = $db_connection->prepare("SELECT id_critique, titre, contenu, date_creation, note FROM critique
-            INNER JOIN user ON critique.id_user = user.id WHERE user.id = :user"); 
-        $db_critiques -> execute([":user" => $user_id]);
-        return $db_critiques;
+
+class Critiques {
+    private $db;
+
+    public function __construct($db_connection){
+        $this->db = $db_connection;
     }
 
-    public function Supprimer(int $critique_id):void{
-        require_once('db.php');
-        $db_critiques = $db_connection->prepare("DELETE FROM critique WHERE id_critique = :id AND id_user = :user"); 
-        $db_critiques -> execute(
-            [":id" => $critique_id,
+    public function Afficher(int $user_id){
+        $stmt = $this->db->prepare("
+            SELECT id_critique, titre, contenu, date_creation, note 
+            FROM critique
+            INNER JOIN user ON critique.id_user = user.id 
+            WHERE user.id = :user
+        ");
+        $stmt->execute([":user" => $user_id]);
+        return $stmt;
+    }
+
+    public function AfficherLikes(int $user_id){
+        $stmt = $this->db->prepare("
+            SELECT *
+            FROM critique
+            INNER JOIN `like` 
+            ON critique.id_critique = `like`.id_critique 
+            WHERE `like`.id_user = :user
+        ");
+        $stmt->execute([":user" => $user_id]);
+        return $stmt;
+    }
+
+    public function Supprimer(int $critique_id){
+        $stmt = $this->db->prepare("
+            DELETE FROM critique 
+            WHERE id_critique = :id AND id_user = :user
+        ");
+        $stmt->execute([
+            ":id" => $critique_id,
             ":user" => $_SESSION['id_user']
         ]);
+        header("Refresh:0");
     }
-}
-
-if (isset($_POST['supprimer'])) {
-    $critiques = new Critiques();
-    $critiques->Supprimer($_POST['id_critique']);
 }
--- dashboard.php
+++ dashboard.php
@@ -12,28 +12,50 @@
 <body>
     <?php require_once("navbar.php"); ?>
     <main>
-        <div class="dashboard"> 
         <?php     
             if (isset($_SESSION["id_user"]) and ($_SESSION["role"]=="critique" or $_SESSION["role"]=="administrateur")){
-                require_once ('crud.php');
+                require_once ('crud.php'); 
+                require_once('db.php');                  
+                $critiques = new Critiques($db_connection);
                 ?>
                 <h2>Bonjour <?=$_SESSION["pseudo"]?></h2>
+                <p> Critiques personnelles : </p>
+                <div class="dashboard">                 
                 <?php
-                $critiques = new Critiques();
-                $critiquesListes = $critiques->Afficher($_SESSION["id_user"])->fetchAll(PDO::FETCH_ASSOC);
-                foreach ($critiquesListes as $critique){ ?>
-                    <article class="articles">
-                        <p> Titre de la critique: <b><?=$critique['titre']?></b></p><br>
-                        <p> Publiée le: <b><?=$critique['date_creation']?></b></p><br>
-                        <p>Note: <?=$critique['note']?>/10</p><br>
-                        <p><?=$critique['contenu']?></p><br>
-                        <form method="post">
-                            <input type="hidden" name="id_critique" value="<?=$critique['id_critique']?>">
-                            <button type="submit" name="supprimer">Supprimer</button>
-                        </form>
-                    </article>
-			<?php }} ?>
-        <div> 
+                    $critiquesListes = $critiques->Afficher($_SESSION["id_user"])->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($critiquesListes as $critique){ ?>
+                        <article class="articles">
+                            <p> Titre de la critique: <b><?=$critique['titre']?></b></p><br>
+                            <p> Publiée le: <b><?=$critique['date_creation']?></b></p><br>
+                            <p>Note: <?=$critique['note']?>/10</p><br>
+                            <p><?=$critique['contenu']?></p><br>
+                            <form method="post">
+                                <input type="hidden" name="id_critique" value="<?=$critique['id_critique']?>">
+                                <button type="submit" name="supprimer">Supprimer</button>
+                            </form>
+                        </article>
+                    <?php } ?>
+                </div> 
+                <p> Critiques likées: </p>
+                <div class="dashboard">                 
+                <?php
+                    $critiquesListes = $critiques->AfficherLikes($_SESSION["id_user"])->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($critiquesListes as $critique){ ?>
+                        <article class="articles">
+                            <p> Titre de la critique: <b><?=$critique['titre']?></b></p><br>
+                            <p> Publiée le: <b><?=$critique['date_creation']?></b></p><br>
+                            <p>Note: <?=$critique['note']?>/10</p><br>
+                            <p><?=$critique['contenu']?></p><br>
+                            <form method="post">
+                                <input type="hidden" name="id_critique" value="<?=$critique['id_critique']?>">
+                                <button type="submit" name=dislike">Retirer le like</button>
+                            </form>
+                        </article>
+                    <?php } ?>
+                </div> 
+            <?php } else { ?>
+                <p>Access denied</p>
+            <?php } ?>
     </main>    
 </body>
 </html>
